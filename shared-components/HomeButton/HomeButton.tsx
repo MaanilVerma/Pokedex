52a696e82756d8c1d673536091963bf6aeaf57e5
Maanil Verma <vermaMaanil97@gmail.com>
2024-04-17 20:51:27 +0530

    feat: add home button with styles

---
+++ shared-components/HomeButton/HomeButton.tsx
@@ -0,0 +1,31 @@
+import { HomeButton as Button } from "@/utils/styles/homeButton";
+import { fetchPokemonList } from "@/api-endpoints/fetchPokemonList";
+import { HomeIcon } from "@/public/static/images/HomeIcon";
+import { HomeButtonProps } from "@/utils/models/homeButton.model";
+
+const HomeButton = (props: HomeButtonProps) => {
+  const handleClick = async () => {
+    props.setLoading(true);
+    props.setDisabledButton(true);
+    props.setPokemonList(await fetchPokemonList(1));
+    props.setLoading(false);
+    props.setDisabledButton(false);
+    props.setPage(1);
+    props.setShowPagination(true);
+  };
+
+  return (
+    <Button
+      className="button"
+      onClick={handleClick}
+      disabled={props.disabledButton ? true : false}
+    >
+      <HomeIcon />
+      Reset
+    </Button>
+  );
+};
+
+export default HomeButton;
+
+//  Scroll Below Icon in Middle or ADD For list scroll below this is a featured pokemon
