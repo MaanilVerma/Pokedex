c876c55ebfaf5c948ededbf2e6ea9da66c5a4db8
Maanil Verma <vermaMaanil97@gmail.com>
2024-04-17 20:59:01 +0530

    chore: integrate fonts & global styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
+import Footer from "@/shared-components/Footer/Footer";
+import StyledComponentsRegistry from "@/utils/lib/registry";
 import { GlobalStyles } from "@/utils/styles/globalStyles";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ["latin"], fallback: ["sans-serif"] });
 
 export const metadata: Metadata = {
-  title: "PokeDex",
+  title: "PokeDex - A Pokemon Collection",
   description: "Pokedex - A Pokemon Collection",
 };
 
@@ -17,9 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <GlobalStyles />
-        {children}
+      <body className={montserrat.className}>
+        <StyledComponentsRegistry>
+          <GlobalStyles />
+          {children}
+          <Footer />
+        </StyledComponentsRegistry>
       </body>
     </html>
   );
